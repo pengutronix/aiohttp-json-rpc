c100f75c534f7bf473ae33a60e75a839d46a51b7
Florian Scherf <f.scherf@pengutronix.de>
2015-11-17 23:19:07 +0100

    add license; add project url; make new release
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@
 from setuptools import setup
 
 setup(name='aiohttp-json-rpc',
-      version='0.1',
+      version='0.1.1',
       author='Florian Scherf',
+      url='https://github.com/pengutronix/aiohttp-json-rpc/tree/doc',
       author_email='f.scherf@pengutronix.de',
+      license='Apache 2.0',
       install_requires=['aiohttp>=0.17.0'],
       packages=['aiohttp_json_rpc'],
       zip_safe=False)
