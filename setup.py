e2d73299b779f4e7d384cb447400bd1973025d53
Florian Scherf <f.scherf@pengutronix.de>
2016-01-27 14:41:57 +0100

    fix dependencies; release 0.2.1.1
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@
 from setuptools import setup
 
 setup(name='aiohttp-json-rpc',
-      version='0.2.1.0',
+      version='0.2.1.1',
       author='Florian Scherf',
       url='https://github.com/pengutronix/aiohttp-json-rpc/',
       author_email='f.scherf@pengutronix.de',
       license='Apache 2.0',
-      install_requires=['aiohttp>=0.17.0'],
+      install_requires=['aiohttp==0.20'],
       packages=['aiohttp_json_rpc'],
       zip_safe=False)
