280dac5eed5c1922965c19ce6f6d5b87322d613f
Florian Scherf <f.scherf@pengutronix.de>
2016-02-08 18:39:33 +0100

    update README and example; release v0.3
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- example.py
+++ example.py
@@ -1,10 +1,7 @@
-# aiohttp-json-rpc
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Florian Scherf <f.scherf@pengutronix.de>
 
-## Installation
-`pip install aiohttp-json-rpc`
-
-## Usage
-```python
 import asyncio
 import aiohttp
 from aiohttp.web import Application, WebSocketResponse
@@ -86,36 +83,3 @@ if __name__ == '__main__':
         loop.run_until_complete(asyncio.wait(tasks))
 
     loop.close()
-```
-
-**Example JSON RPC Messages**
-```
-> {"jsonrpc": "2.0", "method": "list_methods", "id": 1}
-< {"result": ["list_methods", "unsubscribe", "subscribe", "list_subscriptions", "ping"], "id": 1, "jsonrpc": "2.0"}
-
-> {"jsonrpc": "2.0", "method": "ping", "id": 2}
-< {"jsonrpc": "2.0", "method": "ping", "params": "pong", "id": 2}
-
-> {"jsonrpc": "2.0", "method": "subscribe", "params": "clock", "id": 3}
-< {"id": 3, "result": true, "jsonrpc": "2.0"}
-< {"method": "clock", "id": null, "params": "2015-11-17 20:05:46.721194", "jsonrpc": "2.0"}
-< {"method": "clock", "id": null, "params": "2015-11-17 20:05:47.722591", "jsonrpc": "2.0"}
-< {"method": "clock", "id": null, "params": "2015-11-17 20:05:48.724003", "jsonrpc": "2.0"}
-< {"method": "clock", "id": null, "params": "2015-11-17 20:05:49.725479", "jsonrpc": "2.0"}
-< {"method": "clock", "id": null, "params": "2015-11-17 20:05:50.726987", "jsonrpc": "2.0"}
-```
-
-## RPC-Classes
-1. aiohttp_json_rpc.JsonRpc
-  - **Methods:** list_methods
-
-2. aiohttp_json_rpc.PublishSubscribeJsonRpc
-  - **Methods:** list_methods, subscribe, unsubscribe, list_subscriptions
-
-3. aiohttp_json_rpc.django.DjangoAuthJsonRpc
-  - **Methods:** list_methods
-  - checks Django csrf-cookie
-
-4. aiohttp_json_rpc.django.DjangoAuthPublishSubscribeJsonRpc
-  - **Methods:** list_methods, subscribe, unsubscribe, list_subscriptions
-  - checks Django csrf-cookie
