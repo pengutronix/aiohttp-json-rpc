a8fbc58d1878c9057b8e964c384d223c34bd1a26
Florian Scherf <f.scherf@pengutronix.de>
2016-03-16 16:35:47 +0100

    setup: install_requires: version bump
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(name='aiohttp-json-rpc',
       url='https://github.com/pengutronix/aiohttp-json-rpc/',
       author_email='f.scherf@pengutronix.de',
       license='Apache 2.0',
-      install_requires=['aiohttp==0.20'],
+      install_requires=['aiohttp>=0.20,<0.22'],
       packages=['aiohttp_json_rpc'],
       zip_safe=False)
