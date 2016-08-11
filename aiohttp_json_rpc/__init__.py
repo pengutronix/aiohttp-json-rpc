219e210f9f03d8914240bf7f077cd81a7b8e78bf
Florian Scherf <f.scherf@pengutronix.de>
2016-08-11 15:03:29 +0200

    move publish subscribe to base class
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- aiohttp_json_rpc/__init__.py
+++ aiohttp_json_rpc/__init__.py
@@ -1,2 +1,1 @@
-from .base import JsonRpc, RpcInvalidRequestError, RpcInvalidParamsError  # NOQA
-from .publish_subscribe import PublishSubscribeJsonRpc  # NOQA
+from .rpc import JsonRpc, RpcInvalidRequestError, RpcInvalidParamsError  # NOQA
--- aiohttp_json_rpc/rpc.py
+++ aiohttp_json_rpc/rpc.py
@@ -87,6 +87,7 @@ class JsonRpc(object):
     def __init__(self, auth_backend=None):
         self.clients = []
         self.methods = {}
+        self.state = {}
 
         # auth backend
         if not auth_backend:
@@ -301,3 +302,62 @@ class JsonRpc(object):
     @asyncio.coroutine
     def get_methods(self, request):
         return list(request.methods.keys())
+
+    @asyncio.coroutine
+    def subscribe(self, params, ws, **kwargs):
+        if not hasattr(ws, 'topics'):
+            ws.topics = set()
+
+        if type(params) is not list:
+            params = [params]
+
+        for topic in params:
+            if topic and topic not in ws.topics:
+                ws.topics.add(topic)
+
+                if topic in self.state:
+                    ws.send_notification(topic, self.state[topic])
+
+        return True
+
+    @asyncio.coroutine
+    def unsubscribe(self, params, **kwargs):
+        if not hasattr(kwargs['ws'], 'topics'):
+            kwargs['ws'].topics = set()
+
+            return True
+
+        if type(params) is not list:
+            params = [params]
+
+        for topic in params:
+            if topic in kwargs['ws'].topics:
+                kwargs['ws'].topics.remove(topic)
+
+        return True
+
+    @asyncio.coroutine
+    def get_topics(self, params, **kwargs):
+        return list(getattr(kwargs['ws'], 'topics', set()))
+
+    def filter(self, topics):
+        if type(topics) is not list:
+            topics = [topics]
+
+        topics = set(topics)
+
+        for client in self.clients:
+            if not hasattr(client, 'topics'):
+                continue
+
+            if len(topics & client.topics) > 0:
+                yield client
+
+    def notify(self, topic, data=None):
+        if type(topic) is not str:
+            raise ValueError
+
+        self.state[topic] = data
+
+        for client in self.filter(topic):
+            client.send_notification(topic, data)
