2abd526b76d0b7416cbb01794b7fe39e0904a192
Florian Scherf <f.scherf@pengutronix.de>
2018-06-07 16:02:18 +0200

    fix linting error
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- tests/django_project/django_project/settings.py
+++ tests/django_project/django_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-#   'aiohttp_json_rpc.django',
+    # 'aiohttp_json_rpc.django',
     'django_project',
 )
 
