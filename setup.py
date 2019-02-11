2364bf3ae8d3e57dc0e7dde0d7588d55f46a18ff
Florian Scherf <f.scherf@pengutronix.de>
2019-02-11 12:12:10 +0100

    release v0.12
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 __name__ == '__main__' and setup(name='aiohttp-json-rpc',
-      version='0.11.2',
+      version='0.12',
       author='Florian Scherf',
       url='https://github.com/pengutronix/aiohttp-json-rpc/',
       author_email='f.scherf@pengutronix.de',
