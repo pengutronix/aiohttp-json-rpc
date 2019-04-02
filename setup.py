24ec9ae9ae0633b2deaae7e29f6056f150cb4025
Florian Scherf <f.scherf@pengutronix.de>
2019-04-02 17:40:30 +0200

    packaging: enable include_package_data
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 from setuptools import setup, find_packages
 
 __name__ == '__main__' and setup(name='aiohttp-json-rpc',
+      include_package_data=True,
       version='0.12.1',
       author='Florian Scherf',
       url='https://github.com/pengutronix/aiohttp-json-rpc/',
