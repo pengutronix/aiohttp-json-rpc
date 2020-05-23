7e15a22ed01b4aa68a73a0e8e10d88d9b3785062
expert-m <m.sulyak@gmail.com>
2020-05-23 16:01:00 +0700

    Add import JsonRpcClientContext in the main module

--- aiohttp_json_rpc/__init__.py
+++ aiohttp_json_rpc/__init__.py
@@ -1,5 +1,5 @@
 from .decorators import raw_response, validate  # NOQA
-from .client import JsonRpcClient  # NOQA
+from .client import JsonRpcClient, JsonRpcClientContext  # NOQA
 from .rpc import JsonRpc  # NOQA
 
 from .exceptions import (  # NOQA
